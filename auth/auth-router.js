929723ec2563d58c4e1f9040cb85577e4672bdd5
andrewbastian <andrew.bastian@gmail.com>
2020-03-14 12:04:33 -0700

    MVP

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,29 +20,72 @@ router.post("/register", async (req, res, next) => {
 		next(err)
 	}
 })
+//Part I code: w/o tokens
+// router.post("/login", async (req, res, next) => {
+// 	try {
+// 		const { username, password } = req.body
+// 		const user = await Users.findBy({ username }).first()
 
-router.post("/login", async (req, res, next) => {
-	try {
-		const { username, password } = req.body
-		const user = await Users.findBy({ username }).first()
+// 		// since bcrypt hashes generate different results due to the salting,
+// 		// we rely on the magic internals to compare hashes rather than doing it
+// 		// manually with "!=="
+// 		const passwordValid = await bcrypt.compare(password, user.password)
 
-		// since bcrypt hashes generate different results due to the salting,
-		// we rely on the magic internals to compare hashes rather than doing it
-		// manually with "!=="
-		const passwordValid = await bcrypt.compare(password, user.password)
+// 		if (!user || !passwordValid) {
+// 			return res.status(401).json({
+// 				message: "Invalid Credentials",
+// 			})
+// 		}
 
-		if (!user || !passwordValid) {
-			return res.status(401).json({
-				message: "Invalid Credentials",
+// 		res.json({
+// 			message: `Welcome ${user.username}!`,
+// 		})
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+//Part II w tokens:
+
+router.post("/login", async(req, res, next) => {
+    try {
+        const {
+            username,
+            password
+        } = req.body
+        const user = await Users.findBy({
+            username
+        }).first()
+        const passwordValid = await bcrypt.compare(password, user.password)
+
+        if (!user || !passwordValid) {
+            return res.status(401).json({
+                message: "Invalid Credentials",
+            })
+        }
+        req.session.user = user
+
+        res.json({
+            message: `Welcome ${user.username}!`,
+        })
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/logout", restrict(), (req, res, next) => {
+	// this will delete the session in the database and try to expire the cookie,
+	// though it's ultimately up to the client if they delete the cookie or not.
+	// but it becomes useless to them once the session is deleted server-side.
+	req.session.destroy((err) => {
+		if (err) {
+			next(err)
+		} else {
+			res.json({
+				message: "Successfully logged out",
 			})
 		}
-
-		res.json({
-			message: `Welcome ${user.username}!`,
-		})
-	} catch(err) {
-		next(err)
-	}
+	})
 })
 
 module.exports = router
--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,33 +1,35 @@
 const bcrypt = require("bcryptjs")
 const Users = require("../users/users-model")
 
+// from our manual session implementation
+// **********************************************
+// const sessions = {}
+
 function restrict() {
-	// put in variable so we can re-use it
 	const authError = {
 		message: "Invalid credentials",
 	}
 	
 	return async (req, res, next) => {
 		try {
-			const { username, password } = req.headers
-			// make sure the values aren't empty
-			if (!username || !password) {
+//      our manual session implementation
+//      **********************************************
+// 			const { cookie } = req.headers
+// 			if (!cookie) {
+// 				return res.status(401).json(authError)
+// 			}
+// 
+// 			const authToken = cookie.replace("token=", "")
+// 			if (!sessions[authToken]) {
+// 				return res.status(401).json(authError)
+// 			}
+
+			// we set `req.session.user` when the user is authenticated in `/login`.
+			// so we know if it's not set, the user isn't authenticated yet.
+			if (!req.session || !req.session.user) {
 				return res.status(401).json(authError)
 			}
 
-			const user = await Users.findBy({ username }).first()
-			// make sure the user exists
-			if (!user) {
-				return res.status(401).json(authError)
-			}
-
-			const passwordValid = await bcrypt.compare(password, user.password)
-			// make sure the password is correct
-			if (!passwordValid) {
-				return res.status(401).json(authError)
-			}
-
-			// if we reach this point, the user is authenticated!
 			next()
 		} catch(err) {
 			next(err)
@@ -35,4 +37,7 @@ function restrict() {
 	}
 }
 
-module.exports = restrict
+module.exports = {
+	// sessions,
+	restrict,
+}
